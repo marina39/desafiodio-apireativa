4418dc2c242182369c321fc59cf663bb5243acf9
Marina A Gontijo <marina.agontijo@hotmail.com>
2021-04-18 13:48:29 -0300

    Acertando formatações

--- HeroesAPI-main/heroesapi/src/test/java/com/heromanager/heroesapi/HeroesApiApplicationTests.java
+++ HeroesAPI-main/heroesapi/src/test/java/com/heromanager/heroesapi/HeroesApiApplicationTests.java
@@ -1,12 +1,11 @@
 package com.heromanager.heroesapi;
 
-import org.springframework.boot.test.context.SpringBootTest;
-
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import com.digitalinnovation.livecoding.document.Heroes;
-import com.digitalinnovation.livecoding.repository.HeroesRepository;
 import org.junit.runner.RunWith;
+
+import com.heromanager.heroesapi.repository.HeroesRepository;
+
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.http.MediaType;
@@ -14,10 +13,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import java.util.List;
-
-import static com.digitalinnovation.livecoding.constants.HeroesConstant.HEROES_ENDPOINT_LOCAL;
-
+import static com.heromanager.heroesapi.constans.HeroesConstant.HEROES_ENDPOINT_LOCAL;
 
 @RunWith(SpringRunner.class)
 @DirtiesContext
@@ -31,38 +27,28 @@ public class HeroesApiApplicationTests {
   @Autowired
   HeroesRepository heroesRepository;
 
-
   @Test
-  public void getOneHeroeById(){
-
+  public void getOneHeroeById() {
     webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"1")
       .exchange()
       .expectStatus().isOk()
       .expectBody();
-
-
   }
 
   @Test
-  public void getOneHeronotFound(){
-
+  public void getOneHeronotFound() {
     webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"10")
       .exchange()
       .expectStatus().isNotFound();
-
   }
 
-
   @Test
-  public void deleteHero(){
-
+  public void deleteHero() {
     webTestClient.delete().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"1")
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
       .expectStatus().isNotFound()
       .expectBody(Void.class);
-
   }
 
 }
-
